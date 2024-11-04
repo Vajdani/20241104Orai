7397d5bf5215916d88fd21eae30056003e5f87e4
Vajdani <50171679+Vajdani@users.noreply.github.com>
2024-11-04 10:06:02 +0100

    Add files via upload

---
+++ 24_11_04/24_11_04/Program.cs
@@ -0,0 +1,38 @@
+﻿using _24_11_04;
+
+List<Versenyzo> versenyzok = new();
+
+StreamReader r = new("..\\..\\..\\src\\forras.txt", encoding: System.Text.Encoding.UTF8);
+while (!r.EndOfStream)
+{
+    versenyzok.Add(new Versenyzo(r.ReadLine()!));
+}
+r.Close();
+
+Console.WriteLine($"{versenyzok.Count} versenyző vett részt a triatlonon.");    
+Console.WriteLine($"{versenyzok.Count(p => p.Kategoria == "elit")} versenyző vett részt az elit kategórában.");
+Console.WriteLine($"{versenyzok.Count(p => p.Kategoria == "elit junior")} versenyző vett részt az elit junior kategórában.");
+Console.WriteLine($"{versenyzok.Count(p => p.Kategoria == "25-29")} versenyző vett részt a 25-29 kategórában.");
+Console.WriteLine($"A versenyzők átlagéletkora {versenyzok.Average(p => DateTime.Now.Year - p.SzuletesiEv):0.00} év.");
+Console.WriteLine($"A férfi versenyzők átlagéletkora {versenyzok.Where(p => p.Nem).Average(p => DateTime.Now.Year - p.SzuletesiEv):0.00} év.");
+Console.WriteLine($"A női versenyzők átlagéletkora {versenyzok.Where(p => !p.Nem).Average(p => DateTime.Now.Year - p.SzuletesiEv):0.00} év.");
+Console.WriteLine($"A versenyzők kerékpározással töltött összideje: {versenyzok.Sum(p => p.Idok["kerékpár"].TotalHours):0.00} óra");
+Console.WriteLine($"A versenyzők futással töltött összideje: {versenyzok.Sum(p => p.Idok["futás"].TotalHours):0.00} óra");
+Console.WriteLine($"Átlagos úszási idő elit junior kategóriában: {versenyzok.Where(p => p.Kategoria == "elit junior").Average(p => p.Idok["úszás"].TotalMinutes):0.00} perc");
+Console.WriteLine($"Átlagos úszási idő 20-24 kategóriában: {versenyzok.Where(p => p.Kategoria == "20-24").Average(p => p.Idok["úszás"].TotalMinutes):0.00} perc");
+Console.WriteLine($"Átlagos úszási idő elit kategóriában: {versenyzok.Where(p => p.Kategoria == "elit").Average(p => p.Idok["úszás"].TotalMinutes):0.00} perc");
+Console.WriteLine($"A férfi győztes: {versenyzok.Where(p => p.Nem).MinBy(p => p.Idok.Sum(p => p.Value.TotalSeconds))}");
+Console.WriteLine($"A női győztes: {versenyzok.Where(p => !p.Nem).MinBy(p => p.Idok.Sum(p => p.Value.TotalSeconds))}");
+
+var kategoriak = versenyzok.GroupBy(p => p.Kategoria).OrderBy(p => p.Key);
+Console.WriteLine("Korkategóriánként a versenyt befejezők száma(kategória, fő, depó idő):");
+foreach (var item in kategoriak)
+{
+    Console.WriteLine($"\t{item.Key + ":", -11}\t{item.Count(), 2} fő\t{item.Average(p => (p.Idok["I. depó"] + p.Idok["II. depó"]).TotalMinutes):0.00} perc");
+}
+
+Console.WriteLine("Nemenként a versenyt befejezők száma");
+foreach (var item in versenyzok.GroupBy(p => p.Nem).OrderBy(p => p.Key))
+{
+    Console.WriteLine($"\t{(item.Key ? "Férfi" : "Nő")}:\t{item.Count()} fő");
+}
---
+++ 24_11_04/24_11_04/Versenyzo.cs
@@ -0,0 +1,32 @@
+﻿namespace _24_11_04;
+
+internal class Versenyzo
+{
+    public string Nev { get; set; }
+    public int SzuletesiEv { get; set; }
+    public string Rajtszam { get; set; }
+    public bool Nem { get; set; }
+    public string Kategoria { get; set; }
+    public Dictionary<string, TimeSpan> Idok { get; set; }
+
+    public Versenyzo(string line)
+    {
+        string[] split = line.Split(';');
+        Nev = split[0];
+        SzuletesiEv = int.Parse(split[1]);
+        Rajtszam = split[2];
+        Nem = split[3] == "f";
+        Kategoria = split[4];
+
+        Idok = new()
+        {
+            { "úszás",      TimeSpan.Parse(split[5]) },
+            { "I. depó",    TimeSpan.Parse(split[6]) },
+            { "kerékpár",   TimeSpan.Parse(split[7]) },
+            { "II. depó",   TimeSpan.Parse(split[8]) },
+            { "futás",      TimeSpan.Parse(split[9]) },
+        };
+    }
+
+    public override string ToString() => $"[{Rajtszam}] {Nev} ({(Nem ? "Férfi" : "Nő")}, {Kategoria})";
+}
